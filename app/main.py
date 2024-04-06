982ca60294d59106fadd9b49e202503c594bf31a
Oleksandr Mykhailyk <stekloduv45@gmail.com>
2024-04-06 13:26:25 +0200

    completed the task "Washing Station"

--- app/main.py
+++ app/main.py
@@ -16,17 +16,19 @@ class CarWashStation:
 
     def calculate_washing_price(self, car: Car) -> float:
         if car.clean_mark < self.clean_power:
-            return round(car.comfort_class
-                         * (self.clean_power - car.clean_mark)
-                         * self.average_rating / self.distance_from_city_center
-                         , 1)
+            return round(
+                car.comfort_class
+                * (self.clean_power - car.clean_mark)
+                * self.average_rating / self.distance_from_city_center,
+                1
+            )
         return 0
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
-    def serve_cars(self, cars: list) -> float:
+    def serve_cars(self, cars: list) -> list:
         total_income = 0
         for car in cars:
             total_income += self.calculate_washing_price(car)
